5a49229b66823d6c900c339e31aca28f820deb75
Zahra Bashir <zahra_9877@yahoo.com>
2019-03-09 01:02:34 +0330

    Go through types and vars and getting input

--- helloword.go
+++ helloword.go
@@ -1,12 +1,27 @@
 package main
 
-	
-
 import "fmt"
 
-	
-
 func main() {
-    fmt.Println("hello world")
-}
 
+  x := "Hello World"
+
+  x = "first "
+  fmt.Println(x)
+  x = x + "second"
+  fmt.Println(x)
+
+  x  = "hello"
+  var y string = "world"
+  fmt.Println(x == y)
+
+  fmt.Println("Enter a number: ")
+  var input float64
+  fmt.Scanf("%f", &input)
+
+  output := input * 2
+
+  fmt.Println(output)
+
+
+}
