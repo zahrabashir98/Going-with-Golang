6973718730e05523ff181fce532105d01ec73506
Zahra Bashir <zahra_9877@yahoo.com>
2019-03-09 01:23:11 +0330

    Add arrays and figure out dismathings

--- helloword.go
+++ helloword.go
@@ -3,23 +3,37 @@ package main
 import "fmt"
 
 func main() {
-  i := 1
-  for i = 1; i <= 10; i++ {
-    if i % 2 == 0 {
-      fmt.Println(i, "even")
-    } else {
-      fmt.Println(i, "odd")
-    }
-  }
-  fmt.Println(i)
-  switch i {
-    case 0: fmt.Println("Zero")
-    case 1: fmt.Println("One")
-    case 2: fmt.Println("Two")
-    case 3: fmt.Println("Three")
-    case 4: fmt.Println("Four")
-    case 5: fmt.Println("Five")
-    default: fmt.Println("Unknown Number")
-    }
 
+  var x [5]float64
+  x[0] = 98
+  x[1] = 93
+  x[2] = 77
+  x[3] = 82
+  x[4] = 83
+  var total float64 = 0
+for i := 0; i < len(x); i++ {
+  total += x[i]
+}
+fmt.Println(total / 5)
+// or
+fmt.Println(total / float64(len(x)))
+
+/*False statement*/
+// fmt.Println(total / len(x))
+
+total = 0
+for _, value := range x {
+  total += value
+  fmt.Println(value)
+}
+fmt.Println(total / float64(len(x)))
+
+// another form of array declaration
+y := [5]float64{
+  98, 
+  93, 
+  77, 
+  82, 
+  83 }
+fmt.Println(y)
 }
