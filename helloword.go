2d60398a0b02b0555c19c8c88a0656a2b4743dcf
Zahra Bashir <zahra_9877@yahoo.com>
2019-03-09 01:34:18 +0330

    Add slice functions

--- helloword.go
+++ helloword.go
@@ -4,17 +4,12 @@ import "fmt"
 
 func main() {
 
-  // A slice is a segment of an array. Like arrays slices are indexable and have a length
-  var x []float64
-  x = make([]float64, 5)
-  fmt.Println(x)
+    slice1 := []int{1,2,3}
+    slice2 := append(slice1, 4, 5)
+    fmt.Println(slice1, slice2)
+    slice3 := []int{5,6,7}
+    slice4 := make([]int, 5)
+    copy(slice4, slice3)
+    fmt.Println(slice3, slice4)
 
-  // add limitation for array
-  y := make([]float64, 5, 10)
-  fmt.Println(y)
-
-  arr := [5]float64{1,2,3,4,5}
-  z := arr[0:5]
-  fmt.Println(arr)
-  fmt.Println(z)
 }
