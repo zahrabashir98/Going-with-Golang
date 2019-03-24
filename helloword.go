c2e464cd5d372f3ee4cec7a8d9cbecc0ae19282b
zahrabashir98 <zahra_9877@yahoo.com>
2019-03-24 21:26:32 +0430

    Add details of more complicated maps in go

--- helloword.go
+++ helloword.go
@@ -4,11 +4,6 @@ import "fmt"
 
 func main() {
 
-    // Runtime Error: map should be intialized before
-    // var x map[string]int
-    // x["key"] = 10
-    // fmt.Println(x)
-
     x := make(map[string]int)
     x["key"] = 10
     fmt.Println(x["key"])
@@ -19,10 +14,26 @@ func main() {
     name, ok := x["Un"]
     fmt.Println(name, ok)
 
+    // ?
     if name, ok := x["s"]; ok {
         fmt.Println(name, ok)
       }
-      
+
+    // map of maps
+    elements := map[string]map[string]string{
+    "H": map[string]string{
+        "name":"Hydrogen",
+        "state":"gas",
+    },
+    "He": map[string]string{
+        "name":"Helium",
+        "state":"gas",
+    }}
+    fmt.Println(elements["H"]["name"])
+
+    v := [6]string{"a","b","c","d","e","f"}
+    fmt.Println(v[2:5])
+    
       
 
 
